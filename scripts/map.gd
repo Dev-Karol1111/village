f7e872472a277a4d109af261e86b9e78c693f788
dev karol <karol.fojcik2@gmail.com>
2025-09-24 17:05:35 +0200

    Add deleting blocks

--- scripts/map.gd
+++ scripts/map.gd
@@ -15,3 +15,11 @@ func _unhandled_input(event: InputEvent) -> void:
 		var title_cords = Vector2i(int(floor(local_pos.x / 16)), int(floor(local_pos.y / 16)))
 		
 		tilemap_layer.set_cell(title_cords, block[1] , block[0])
+	
+	elif event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_RIGHT: 
+		var mouses_pos = get_global_mouse_position()
+		var local_pos = tilemap_layer.to_local(mouses_pos)
+		
+		var title_cords = Vector2i(int(floor(local_pos.x / 16)), int(floor(local_pos.y / 16)))
+		
+		tilemap_layer.set_cell(title_cords, 0, Vector2i(1,0))
